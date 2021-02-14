b361733dc157de8da8861e4e78d8d59bd47dde6a
Akiacode <catry.me@gmail.com>
2021-02-14 22:21:05 +0900

    make cli

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -13,18 +13,37 @@ pub fn cli(str: Vec<String>) {
             let mut file = File::create(".boba.json").expect("파일 생성에 문제 있음");
             let _ = file.write_all(
 r#"{
-    "outDir": "" 
+    "outDir": "",
+    "inDir": ""
 }"#.as_bytes());
             println!("Config done.")
         },
-        "build" => (),
+        "build" => {
+            //cargo run build ./html.boba ./html.html
+            let mut file = File::open(str[2].to_string()).expect("메세지 부분에 오류 남");
+            let mut boba = String::new();
+            let _ = file.read_to_string(&mut boba);
+            let mut file = File::create(str[3].clone()).expect("파일 생성에 문제 있음");
+            let _ = file.write_all(Lexar::new(boba).as_bytes());
+            println!("Done.")
+        },
         "run" => {
             //cargo run run ./html.boba
             let mut file = File::open(str[2].to_string()).expect("메세지 부분에 오류 남");
             let mut boba = String::new();
             let _ = file.read_to_string(&mut boba);
-            Lexar::new(boba);
-        }
-        _ => (),
+            println!("{}", Lexar::new(boba));
+        },
+        "help" | _ => {
+            println!("Commands List:");
+            println!("\tboba init - Make Config File");
+            println!("\tex)\t>boba init");
+            println!("\tboba build [filename].boba [filename].html - Build boba");
+            println!("\tex)\t>boba build ./test.boba ./test.html");
+            println!("\tboba run [filename].boba - Show Build Result");
+            println!("\tex)\t>boba run ./test.boba");
+            println!("\tboba help | [Somethings] - Show Commands List");
+            println!("\tex)\t>boba help");
+        },
     }
 }
--- src/compiler/lexar.rs
+++ src/compiler/lexar.rs
@@ -1,3 +1,4 @@
+use super::token::Token;
 pub struct Lexar;
 
 /**
@@ -7,34 +8,48 @@ pub struct Lexar;
  */
 
 impl Lexar {
-    pub fn new(string: String) {
+    pub fn new(string: String) -> String {
         let mut tokens= vec![];
         for i in string.lines() {
             if i.is_empty() { continue; }
             let mut tag = i.split(".").nth(0).unwrap(); // 0. [html] | 1. [lang="asdasd",dasd="asdasd"]
-            println!("{}", i);
+
             if tag.contains("\"") { 
                 // 'p' "This is mypage" .p
                 if !i.split("\"").nth(0).unwrap().is_empty() {
-                    tokens.push(i.split("\"").nth(0).unwrap().trim());
+                    tokens.push(Token { keyword: String::from("TAG"), value: i.split("\"").nth(0).unwrap().trim().to_string() });
+                    tokens.push(Token { keyword: String::from("MSG"), value: i.split("\"").nth(1).unwrap().trim().to_string() });
+                    //"[This is mypage]"
                 }
-                // . 찾기
-                tag = i.trim().split("\"").nth(2).unwrap_or("None"); // "." e.g p "This is mypage" '.p'
+                tag = i.trim().split("\"").nth(2).unwrap_or("None"); // "." e.g p "This is mypage" [.p]
                 if tag == "None" {
                     continue;
                 }
             } else if tag.trim().is_empty() { 
                 tag = i;
             }
-            tokens.push(tag.trim());
+            if !tag.is_empty() {
+                tokens.push(Token { keyword: String::from("TAG"), value: tag.trim().to_string() });
+                let data = i.trim().split(".").nth(1).unwrap_or("None");
+                if data != "None" { // html.[lang="en-US",asda="asd"]
+                    tokens.push(Token { keyword: String::from("DATA"), value: data.trim().to_string() });
+                }
+            }
         }
-        println!("{:?}", tokens);
+        return parse(tokens);
     }
 }
 
-pub fn parse(string: &str) -> String {
+pub fn parse(tokens: Vec<Token>) -> String {
     let mut html = String::new();
     html.push_str("<!DOCTYPE html>");
-
+    for (_n, i) in tokens.iter().enumerate() {
+        if i.value.is_empty() { continue; }
+        if i.keyword == "TAG" {
+            html.push_str( &("<".to_string() + &i.value.to_string() + ">"));
+        } else if i.keyword == "MSG" {
+            html.push_str(&i.value);
+        }
+    }
     return html;
 }
